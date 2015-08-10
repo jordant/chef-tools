2a6fd5765c87fb9bd542da137e4d6fa02d8e2b23
Jordan Tardif <jordan@dreamhost.com>
2015-08-10 23:55:39 +0000

    Adding setup/requirements files

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Python chef tools',
+      author='Jordan Tardif',
+      author_email='jordan.tardif@gmail.com',
+      url='https://github.com/jordant/chef-tools.git',
+      scripts=['fork-ssh.py'],
+      )
